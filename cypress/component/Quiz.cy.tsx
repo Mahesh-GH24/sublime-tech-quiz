545229235e157b65bcf9355ba478c23bb134572f
Mahesh Gunasekaran <maheshgunasekaran24@gmail.com>
2025-02-05 00:50:40 -0600

    Added different test scenarios for component testing related to Quiz

--- cypress/component/Quiz.cy.tsx
+++ cypress/component/Quiz.cy.tsx
@@ -10,12 +10,68 @@ describe('<Quiz />', () => {
   })
 })
 
-//Component Test 2 
+//Component Test 2 - Intercept using the questions from fixtures - questions.json
 
-// describe('<Quiz />', () => {
-//   beforeEach(() => {
-//     cy.intercept('GET')
-//   })
+describe('<Quiz />', () => {
+  beforeEach(() => {
+    cy.intercept({
+      method: "GET",
+      url: "/api/questions/random"
+    },
+    {
+      fixture: 'questions.json',
+      statusCode: 200
+    })
+  });
+});
 
-// }
+it('renders', () => {
+  cy.mount(<Quiz />)
+})
+
+it('mounts and displays the "Start Quiz" button', () => {
+  //Arrange
+  cy.mount(<Quiz />);
+
+  //Act
+
+  //Assert
+  cy.get('button').contains('Start Quiz').should("be.visible");
+});
+
+it('should display the first question when the "start quiz" button is clicked', () => {
+  //Arrange
+  cy.mount(<Quiz />);
+
+  //ACT
+  cy.get('button').contains('Start Quiz').click();
+
+  //Assert
+  cy.get('h2').contains("Which is the correct answer?").should("be.visible");
+});
+
+it('should display four choices for the first question when the "start quiz" button is clicked', () => {
+  
+  //Arrange
+  cy.mount(<Quiz />);
+
+  //ACT
+  cy.get('button').contains('Start Quiz').click();
+
+  //Assert
+  cy.get('alert').contains("Correct").should("be.visible");
+  cy.get('alert').contains("Absolutely InCorrect").should("be.visible");
+  cy.get('alert').contains("Nope").should("be.visible");
+  cy.get('alert').contains("Still No").should("be.visible");
+
+
+
+
+});
+
+
+
+
+
+
 
